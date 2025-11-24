ebe17d6d11f8e060348c775a19c248dc7bffdf44
Thanh Phong Le <ltphongssvn@gmail.com>
2025-11-24 07:29:55 -0800

    feat: Add Turbo Stream broadcasts to VitalSign model

--- app/models/vital_sign.rb
+++ app/models/vital_sign.rb
@@ -1,3 +1,10 @@
+# app/models/vital_sign.rb
 class VitalSign < ApplicationRecord
   belongs_to :patient
+  
+  validates :recorded_at, presence: true
+  
+  broadcasts_to :patient
+  
+  after_create_commit -> { broadcast_prepend_to patient, target: "vital_signs_list" }
 end
