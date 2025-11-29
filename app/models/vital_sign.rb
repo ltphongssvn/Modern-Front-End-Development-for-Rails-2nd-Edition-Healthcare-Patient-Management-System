593a03c386e29b9ec6a5dba5c8f74a6c0af7b700
Thanh Phong Le <ltphongssvn@gmail.com>
2025-11-29 06:59:22 -0800

    fix: Remove broadcasts from VitalSign model

--- app/models/vital_sign.rb
+++ app/models/vital_sign.rb
@@ -3,8 +3,4 @@ class VitalSign < ApplicationRecord
   belongs_to :patient
   
   validates :recorded_at, presence: true
-  
-  broadcasts_to :patient
-  
-  after_create_commit -> { broadcast_prepend_to patient, target: "vital_signs_list" }
 end
