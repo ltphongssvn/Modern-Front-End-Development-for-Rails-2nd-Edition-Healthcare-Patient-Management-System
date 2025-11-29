2e59282ad47c3a2b94d751895a7e11d47639f937
Thanh Phong Le <ltphongssvn@gmail.com>
2025-11-29 06:58:22 -0800

    fix: Remove broadcasts_to from Appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,6 +5,4 @@ class Appointment < ApplicationRecord
   enum status: { scheduled: 0, confirmed: 1, completed: 2, cancelled: 3 }
   
   validates :scheduled_at, presence: true
-  
-  broadcasts_to :patient
 end
